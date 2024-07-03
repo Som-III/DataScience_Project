8136fae8f6bd0ceb9640ea4eb96e629b92efc462
Som-III <sommoroliyaPP@gmail.com>
2024-07-03 17:07:45 +0530

    utils added

---
+++ src/DataSc_project/utils/helpers.py
@@ -0,0 +1,97 @@
+import os
+import yaml
+from logger import logger
+import json
+import joblib
+from pathlib import Path
+from typing import Dict,Any
+
+
+
+def read_yaml(path_to_yaml: Path) -> Dict[str:Any]:
+    """reads yaml file and returns
+
+    Args:
+        path_to_yaml (str): path like input
+
+    Raises:
+        ValueError: If the YAML file is empty.
+        Exception: For other exceptions.
+
+    Returns:
+        Dict[str, Any]: Content of the YAML file.
+    """
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            if content is None:
+                raise ValueError("YAML file is empty")
+            logger.info(f"YAML file: {path_to_yaml} loaded successfully")
+            return content
+    except ValueError as ve:
+        raise ve        
+    except Exception as e:
+        raise e
+
+
+
+    
+def create_directories(path_to_directories: list, verbose=True):
+    """create list of directories
+
+    Args:
+        path_to_directories (list): list of path of directories
+        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
+    """
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"created directory at: {path}")
+
+
+
+
+def save_json(path: Path, data: dict):
+    """save json data
+
+    Args:
+        path (Path): path to json file
+        data (dict): data to be saved in json file
+    """
+    with open(path, "w") as f:
+        json.dump(data, f, indent=4)
+
+    logger.info(f"json file saved at: {path}")
+
+
+
+
+def load_json(path: Path) -> dict:
+    """load json files data
+
+    Args:
+        path (Path): path to json file
+
+    Returns:
+        dict: data as dict
+    """
+    with open(path) as f:
+        content = json.load(f)
+
+    logger.info(f"json file loaded succesfully from: {path}")
+    return content
+#########check load json return
+
+
+
+def get_size(path: Path) -> str:
+    """get size in KB
+
+    Args:
+        path (Path): path of the file
+
+    Returns:
+        str: size in KB
+    """
+    size_in_kb = round(os.path.getsize(path)/1024)
+    return f"~ {size_in_kb} KB"
